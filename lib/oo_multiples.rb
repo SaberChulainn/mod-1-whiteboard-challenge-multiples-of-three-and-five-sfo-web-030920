0c4950bca1a0548dd3704bb7fa460d559e9db48f
Ty Bandachit <tbandachit@gmail.com>
2020-03-26 16:30:09 +0000

    Automatically backed up by Learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,13 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_reader = number
+  def initialize(number)
+    @numer = number
+  end
+  
+  def collect_multiples
+    
+    
+  end
+  
+end
